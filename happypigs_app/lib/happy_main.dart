f0d7f0afbb54e9bd8c86a7cdddeb9694e38f0b6a
m1raedoraemi <nellmi1188@gmail.com>
2021-06-25 16:05:25 +0900

    create table Plate

--- happypigs_app/lib/happy_main.dart
+++ happypigs_app/lib/happy_main.dart
@@ -2,6 +2,8 @@
 
 import 'package:flutter/material.dart';
 
+import 'db/db_manager.dart';
+
 class HappyMainPage extends StatefulWidget {
   @override
   _HappyMainPageState createState() => _HappyMainPageState();
@@ -10,7 +12,13 @@ class HappyMainPage extends StatefulWidget {
 class _HappyMainPageState extends State<HappyMainPage> {
   @override
   Widget build(BuildContext context) {
+    load_Db();
     return Scaffold(
     );
+  }
+
+  Future<void> load_Db() async{
+    await init_db();
+    await printDatabase();
   }
 }
