a43e92e6d43639c1e7fd78bc150d6e4ad1b7c9e1
Gayoung <Gayoung@MacBookPro.local>
2021-07-23 19:35:16 +0900

    Upload single picture version of addPlate page

---
+++ happypigs_app/lib/file.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+import 'dart:ui';
+
+import 'package:photo_manager/photo_manager.dart';
+
+class FileModel {
+  List<dynamic> files=[];
+  String folder;
+
+  FileModel(this.files, this.folder);
+
+}
